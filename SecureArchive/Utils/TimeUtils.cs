ec0f194a6b83de1729c9a329cfb1d3946c381df6
toyota-m2k <toyota.mitsuki@gmail.com>
2023-08-24 21:20:59 +0900

    feat: db recovery

---
+++ SecureArchive/Utils/TimeUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace SecureArchive.Utils;
+public static class TimeUtils {
+    private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
+    /**
+     * DateTimeを Java の Date#time の値に変換する。
+     */
+    public static long dateTime2javaTime(DateTime time) {
+        TimeSpan span = time - Epoch;
+        return (long)Math.Round(span.TotalMilliseconds);
+    }
+}
